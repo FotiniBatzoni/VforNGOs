59daebd22e96f0aac8a01c8251ca4766ddce17ba
fotini <f.batzoni@gmail.com>
2022-11-27 14:31:02 +0200

    Test   EmailIsValid_MissingDotPart

--- VforNGOss.Tests/Utilities/ValidateEmail.cs
+++ VforNGOss.Tests/Utilities/ValidateEmail.cs
@@ -3,21 +3,28 @@ using VforNGOss.Utilities;
 
 namespace VforNGOss.Tests
 {
+    [TestFixture,Description("Validating Email")]
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
-        [Test]
-        public void EmailIsValidTest()
+        [Test,Description("The given email is in correct format")]
+        public void EmailIsValid()
         {
             string email = "babis@gmail.com";
 
             bool isValid = ValidateEmail.EmailIsValid(email);
 
             Assert.AreEqual(true, isValid);
+
+        }
+
+        [Test, Description("The given email is in incorrect format .com/en/etc is missing")]
+        public void EmailIsValid_MissingDotPart()
+        {
+            string email = "babis@gmail";
+
+            bool isValid = ValidateEmail.EmailIsValid(email);
+
+            Assert.AreEqual(false, isValid);
 
         }
     }
