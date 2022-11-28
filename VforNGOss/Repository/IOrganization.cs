447878e99181d9efe03cc785dc9b167746e75838
fotini <f.batzoni@gmail.com>
2022-11-28 15:49:09 +0200

    Creating IOrganization Interfafe

---
+++ VforNGOss/Repository/IOrganization.cs
@@ -0,0 +1,14 @@
+﻿using VforNGOss.Models;
+
+namespace VforNGOss.Repository
+{
+    public interface IOrganization
+    {
+        IList<Organization> GetOrganizations();
+        Organization GetOrganizationById(int? id);
+        void InsertNew(Organization organization);
+        void Update(Organization organization);
+        void Delete(Organization organization);
+
+    }
+}
