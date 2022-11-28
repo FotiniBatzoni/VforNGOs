9ff50cc85afe74ed2e532820b946f917e8f5093a
fotini <f.batzoni@gmail.com>
2022-11-28 15:54:59 +0200

    Creating IVolunteer interface

---
+++ VforNGOss/Repository/IRepository/IVolunteer.cs
@@ -0,0 +1,14 @@
+﻿using VforNGOss.Models;
+
+namespace VforNGOss.Repository
+{
+    public interface IVolunteer
+    {
+        IList<Volunteer> GetVolunteers();
+        Volunteer GetVolunteerById(int? id);
+        void InsertNew(Volunteer volunteer);
+        void Update(Volunteer volunteer);
+        void Delete(Volunteer volunteer);
+
+    }
+}
