73488356a15db3a97473d873c187d2986d65c353
fotini <f.batzoni@gmail.com>
2022-11-29 14:40:11 +0200

    Implementing Dapper ORM

---
+++ VforNGOss/DataAccessLayer/DatabaseConnectionDapper/DapperContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.SqlClient;
+
+namespace VforNGOss.DataAccessLayer.DatabaseConnectionDapper
+{
+    public class DapperContext
+    {
+        private readonly IConfiguration _configuration;
+        public DapperContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public IDbConnection CreateConnection()
+            => new SqlConnection(_configuration.GetConnectionString("SqlConnection"));
+    }
+}
--- VforNGOss/Program.cs
+++ VforNGOss/Program.cs
@@ -1,7 +1,14 @@
+using VforNGOss.DataAccessLayer.DatabaseConnectionDapper;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//For Dapper
+builder.Services.AddSingleton<DapperContext>(); // Add this line to register the service
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
 
 var app = builder.Build();
 
@@ -12,6 +19,8 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
