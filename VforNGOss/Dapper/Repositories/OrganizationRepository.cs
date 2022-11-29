54e79d1cae6108e9eb8151bc81550cb74f0eda8b
fotini <f.batzoni@gmail.com>
2022-11-29 15:52:54 +0200

    Creating OrganizationRepository

---
+++ VforNGOss/Dapper/Repositories/OrganizationRepository.cs
@@ -0,0 +1,33 @@
+﻿using Dapper;
+using VforNGOss.DataAccessLayer.DatabaseConnectionDapper;
+using VforNGOss.Models;
+
+namespace VforNGOss.Dapper.Repositories
+{
+    public class OrganizationRepository : IOrganizationRepository
+    {
+        private readonly DapperContext _context;
+        public OrganizationRepository(DapperContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<Organization>> GetAll()
+        {
+            string sqlQuery = "SELECT * FROM Organizations";
+            using (var connection = _context.CreateConnection())
+            {
+                var organizations = await connection.QueryAsync<Organization>(sqlQuery);
+                return organizations.ToList();
+            }
+        }
+
+        public async Task<Organization> GetById(int id)
+        {
+            string sqlQuery = "SELECT * FROM Organizations WHERE Id = @Id";
+            using (var connection = _context.CreateConnection())
+            {
+                return await connection.QuerySingleAsync<Organization>(sqlQuery, new { Id = id });
+            }
+        }
+    }
+}
