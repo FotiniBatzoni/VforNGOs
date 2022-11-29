e792924aef890190e4ce469b351da2ad420aebda
fotini <f.batzoni@gmail.com>
2022-11-29 15:05:05 +0200

    Creating A Dapper folder and Item class

---
+++ VforNGOss/Dapper/Entities/Item.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VforNGOss.Dapper.Entities
+{
+    public class Item
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+    }
+}
---
+++ VforNGOss/Dapper/Repositories/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using VforNGOss.DataTransefObjects;
+
+namespace VforNGOss.Repositories
+{
+    public interface IRepository
+    {
+        public Task Create(CreateDTO createDTO, int Id);
+
+        public Task<IEnumerable<TodoItem>> GetAll();
+
+        public Task<TodoItem> GetById(Guid id);
+
+        public Task<IEnumerable<TodoItem>> GetByUser(Guid id);
+
+        public Task Update(UpdateTodoDTO projectDTO, Guid id);
+
+        public Task Delete(Guid id);
+    }
+}
