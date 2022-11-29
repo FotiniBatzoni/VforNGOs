8d931ebc039b09330f5f8694fa1eccf88bc78074
fotini <f.batzoni@gmail.com>
2022-11-29 15:55:24 +0200

    Creating VolunteerRepository

---
+++ VforNGOss/Dapper/Repositories/VolunteerRepository.cs
@@ -0,0 +1,34 @@
+﻿using Dapper;
+using VforNGOss.DataAccessLayer.DatabaseConnectionDapper;
+using VforNGOss.Models;
+
+namespace VforNGOss.Dapper.Repositories
+{
+    public class VolunteerRepository : IVolunteerRepository
+    {
+        private readonly DapperContext _context;
+        public VolunteerRepository(DapperContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<Volunteer>> GetAll()
+        {
+            string sqlQuery = "SELECT * FROM Volunteers";
+            using (var connection = _context.CreateConnection())
+            {
+                var organizations = await connection.QueryAsync<Volunteer>(sqlQuery);
+                return organizations.ToList();
+            }
+        }
+
+        public async Task<Volunteer> GetById(int id)
+        {
+            string sqlQuery = "SELECT * FROM Organizations WHERE Id = @Id";
+            using (var connection = _context.CreateConnection())
+            {
+                return await connection.QuerySingleAsync<Volunteer>(sqlQuery, new { Id = id });
+            }
+        }
+    }
+
+}
