1b96b93016397a6985b42ccbe0bb053d0fe65bcb
fotini <f.batzoni@gmail.com>
2022-12-05 20:02:44 +0200

    Create Haser Class

---
+++ VforNGOss/Utilities/SecurePasswordHasher.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System.Security.Cryptography;
+
+public static class Hasher
+{
+    public static string HashPassword(string password)
+    {
+    byte[] salt = RandomNumberGenerator.GetBytes(128 / 8); // divide by 8 to convert bits to bytes
+
+    // derive a 256-bit subkey (use HMACSHA256 with 100,000 iterations)
+    string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+        password: password!,
+        salt: salt,
+        prf: KeyDerivationPrf.HMACSHA256,
+        iterationCount: 100000,
+        numBytesRequested: 256 / 8));
+
+    return hashed;
+    }
+}
