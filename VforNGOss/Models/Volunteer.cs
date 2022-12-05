93501738dc44e1b7fe02a7b31ae16f4b1d1d39f7
fotini <f.batzoni@gmail.com>
2022-12-05 16:11:46 +0200

    Adding Fields to Volunteer Model

--- VforNGOss/Models/Volunteer.cs
+++ VforNGOss/Models/Volunteer.cs
@@ -8,6 +8,18 @@ namespace VforNGOss.Models
         public int Id { get; set; }
         [Required]
         public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public int Age { get; set; }
+
+        public string City { get; set; }
+
+        public string Education { get; set; }
+
+
+
+
     }
 }
 
