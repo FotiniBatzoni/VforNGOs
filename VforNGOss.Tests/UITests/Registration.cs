bdf92d01698c17345ad7005e1838017666649ae0
fotini <f.batzoni@gmail.com>
2022-12-08 17:04:10 +0200

    First Selenium test

--- VforNGOss.Tests/UITests/Registration.cs
+++ VforNGOss.Tests/UITests/Registration.cs
@@ -8,20 +8,31 @@ namespace VforNGOss.Tests.UITests
     [TestFixture, Description("Registration Process")]
     public class Registration
     {
+        IWebDriver driver;
+
+        [OneTimeSetUp]
+        public void StartChrome()
+        {
+            driver = new ChromeDriver();
+        }
+
         [Test, Description("Reguister a User")]
         public void RegisterAUser()
         {
-            using (WebDriver driver  = new ChromeDriver())
-            {
-               
-                //driver.Url = "https://localhost:7211/";
-                driver.Navigate().GoToUrl("https://localhost:7211/");
+            driver.Navigate().GoToUrl("https://localhost:7211/");
+            Assert.Pass();
                 //driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
                 //IWebElement AreUOrg = driver.FindElement(By.Id("ruorg"));
                 //AreUOrg.Click();
                 //Console.WriteLine("lala");
-            }    
+             
 
+        }
+
+        [OneTimeTearDown]
+        public void CloseTest()
+        {
+          // driver.Close();
         }
     }
 }
