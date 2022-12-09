a42f2f5af8850f39095545c3de75b41e2a9bb94e
fotini <f.batzoni@gmail.com>
2022-12-09 12:04:32 +0200

    Testing Display Organizations

---
+++ VforNGOss.Tests/UITests/Display.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace VforNGOss.Tests.UITests
+{
+    [TestFixture, Description("Display")]
+    public class Display
+    {
+        IWebDriver driver;
+
+        [OneTimeSetUp]
+        public void StartChrome()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [Test, Description("Display an Organization")]
+        public void DisplayAnOrganization()
+        {
+            driver.Navigate().GoToUrl("https://localhost:7211");
+            IWebElement Display = driver.FindElement(By.Id("display"));
+            Display.Click();
+            IWebElement Org = driver.FindElement(By.Id("org"));
+            Org.Click();
+            Assert.Pass();
+        }
+
+        //[Test, Description("Register a Volunteer")]
+        //public void RegisterAVolunteer()
+        //{
+        //    driver.Navigate().GoToUrl("https://localhost:7211");
+        //    IWebElement AreUVol = driver.FindElement(By.Id("ruvol"));
+        //    AreUVol.Click();
+        //    Assert.Pass();
+        //}
+
+        [OneTimeTearDown]
+        public void CloseTest()
+        {
+          driver.Close();
+        }
+    }
+}
